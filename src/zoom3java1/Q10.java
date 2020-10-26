7b918d545dde4032729074403d670dd560fdfec0
SayerAljohani <sayeeer1409@hotmail.com>
2020-10-26 11:29:36 +0300

    add question 10

---
+++ src/zoom3java1/Q10.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoom3java1;
+
+/**
+ *
+ * @author sayee
+ */
+public class Q10 {
+    //Q10. Write a java program for printing the sum and average of the variables
+    //x, y and z. Print the result with four digits after the decimal point?
+}
