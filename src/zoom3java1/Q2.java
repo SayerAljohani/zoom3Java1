3f12728b6aa0e8a27b80468c51605b56407cc5c1
SayerAljohani <sayeeer1409@hotmail.com>
2020-10-26 11:30:27 +0300

    add question 2

--- src/zoom3java1/Q2.java
+++ src/zoom3java1/Q2.java
@@ -10,6 +10,8 @@ package zoom3java1;
  * @author sayee
  */
 public class Q2 {
+    /*Q2. Write a java program for printing the average of the variables x, y and z. 
+    Print the result with two digits after the decimal point? */
     public static void main(String[] args) {
     double x=5,y=9,z=6;
         System.out.printf("%.2f %n",(x+z+y)/3);
